9cbed1fd93b5edf14701d9a09786d3460dea53c0
Andrei Pereverzev <andyper@rambler.ru>
2022-08-31 19:41:08 +0700

    SalesManager and Main classes added

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] sls = {700, 400, 11, 1000, 1200};
+        SalesManager sM = new SalesManager(sls);
+
+        System.out.println(sM.max(5));
+    }
+}
