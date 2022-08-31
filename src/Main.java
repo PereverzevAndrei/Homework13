3818b591c88eb74821da2f8d36d8d0409fe69f10
Andrei Pereverzev <andyper@rambler.ru>
2022-08-31 20:57:07 +0700

    Method AverageSale added

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        long[] sls = {700, 400, 11, 1000, 1200};
-        SalesManager sM = new SalesManager(sls);
 
-        System.out.println(sM.max(5));
+        int[] sls = {700, 400, 11, 1000, 1200};
+        SalesManager sM = new SalesManager(sls);
+        SalesManager.AverageSale(sls);
+
     }
 }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -1,18 +1,37 @@
 public class SalesManager {
-    protected long[] sales;
+    protected static int[] sales;
 
-    public SalesManager(long[] sales) {
+        public SalesManager(int[] sales) {
         this.sales = sales;
     }
 
-    public long max(int i) {
-        long max = -1;
-        for (long sale : sales) {
+    public static int max() {
+        int max = -1;
+        for (int sale : sales) {
             if (sale > max) {
                 max = sale;
             }
         }
         return max;
+    }
+
+    public static int min() {
+        int min = max();
+        for (int sale : sales) {
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+
+    public static int AverageSale(int[] sales) {
+        int salesSumm = 0;
+        for (int sale : sales) {
+            salesSumm += sale;
+        }
+        System.out.println("Усредненная продажа: " + (salesSumm - min() - max()) / (sales.length - 2));
+        return salesSumm;
     }
 }
 
